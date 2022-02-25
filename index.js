2a5a23a2f9af488bb12d048755da0f47f7b0a211
jydeveloper <lukaryu@naver.com>
2022-02-25 16:54:39 +0900

    api routes 생성, 404 에러 안나게 route 연결

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const authRoute = require('./routes/auth');
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ mongoose
 	.connect(process.env.MONGO_URL)
 	.then(() => console.log('DB Connection Success!'))
 	.catch((err) => console.log(err));
+
+app.use('/api/auth', authRoute);
 
 app.listen(8800, () => {
 	console.log('Backend server is running!');
